9792046fad02e2a4e1aa882a49d19bbb3815c29c
hamresan <pourshahmir@gmail.com>
2026-09-23 21:12:24 +0400

    feat: expose deduplication schema validator

--- src/csv_data_cleaner/application/deduplication/__init__.py
+++ src/csv_data_cleaner/application/deduplication/__init__.py
@@ -1,5 +1,5 @@
 """Application deduplication components."""
 
-from csv_data_cleaner.application.deduplication.row_deduplicator import RowDeduplicator
+from csv_data_cleaner.application.deduplication.deduplication_schema_validator import (\n    DeduplicationSchemaValidator,\n)\nfrom csv_data_cleaner.application.deduplication.row_deduplicator import RowDeduplicator
 
-__all__ = ["RowDeduplicator"]
+__all__ = ["DeduplicationSchemaValidator", "RowDeduplicator"]
