2024f9cc49fd34f41ae038ab08261b569afee0b6
hamresan <pourshahmir@gmail.com>
2026-09-23 21:47:53 +0400

    test: update transform dependency injection

--- tests/unit/application/processing/test_dataset_processor.py
+++ tests/unit/application/processing/test_dataset_processor.py
@@ -16,10 +16,12 @@ from csv_data_cleaner.application.normalization import (
 )
 from csv_data_cleaner.application.processing import DatasetProcessor, RowProcessor
 from csv_data_cleaner.application.transforms import (
+    FilterRuleEvaluator,
     FilterSchemaValidator,
     FilterSortProcessor,
     RowFilter,
     RowSorter,
+    SortValueKeyBuilder,
     SortSchemaValidator,
 )
 from csv_data_cleaner.application.validation import (
@@ -60,7 +62,7 @@ def build_processor() -> DatasetProcessor:
         ),
         row_deduplicator=RowDeduplicator(),
         deduplication_schema_validator=DeduplicationSchemaValidator(),
-        filter_sort_processor=FilterSortProcessor(RowFilter(), RowSorter()),
+        filter_sort_processor=FilterSortProcessor(RowFilter(FilterRuleEvaluator()), RowSorter(SortValueKeyBuilder())),
         filter_schema_validator=FilterSchemaValidator(),
         sort_schema_validator=SortSchemaValidator(),
     )
