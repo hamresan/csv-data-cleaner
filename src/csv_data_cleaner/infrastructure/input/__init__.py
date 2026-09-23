186ecc7e8a20e6c301826abe5b7210d211a285f8
hamresan <pourshahmir@gmail.com>
2026-09-23 17:51:50 +0400

    refactor: expose focused infrastructure components

--- src/csv_data_cleaner/infrastructure/input/__init__.py
+++ src/csv_data_cleaner/infrastructure/input/__init__.py
@@ -1,5 +1,6 @@
 """Tabular input adapter public API."""
 
+from csv_data_cleaner.infrastructure.input.data_frame_mapper import DataFrameInputMapper
 from csv_data_cleaner.infrastructure.input.pandas_input_reader import PandasInputReader
 
-__all__ = ["PandasInputReader"]
+__all__ = ["DataFrameInputMapper", "PandasInputReader"]
