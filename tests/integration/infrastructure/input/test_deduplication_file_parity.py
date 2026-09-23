2da936cc8c0618407d8139d19ed0340aad5d8246
hamresan <pourshahmir@gmail.com>
2026-09-23 21:47:56 +0400

    test: update transform dependency injection

--- tests/integration/infrastructure/input/test_deduplication_file_parity.py
+++ tests/integration/infrastructure/input/test_deduplication_file_parity.py
@@ -20,10 +20,12 @@ from csv_data_cleaner.application.normalization import (
 )
 from csv_data_cleaner.application.processing import DatasetProcessor, RowProcessor
 from csv_data_cleaner.application.transforms import (
+    FilterRuleEvaluator,
     FilterSchemaValidator,
     FilterSortProcessor,
     RowFilter,
     RowSorter,
+    SortValueKeyBuilder,
     SortSchemaValidator,
 )
 from csv_data_cleaner.application.validation import (
@@ -105,7 +107,7 @@ def build_processor() -> DatasetProcessor:
         ),
         row_deduplicator=RowDeduplicator(),
         deduplication_schema_validator=DeduplicationSchemaValidator(),
-        filter_sort_processor=FilterSortProcessor(RowFilter(), RowSorter()),
+        filter_sort_processor=FilterSortProcessor(RowFilter(FilterRuleEvaluator()), RowSorter(SortValueKeyBuilder())),
         filter_schema_validator=FilterSchemaValidator(),
         sort_schema_validator=SortSchemaValidator(),
     )
